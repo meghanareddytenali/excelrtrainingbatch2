a6ccf4abf757c9759dbf86c8a9978c3ac849d41c
meghanareddytenali <tenalimeghana000@gmail.com>
2025-01-02 12:16:12 +0530

    Add files via upload

---
+++ Training batch2/excelrtrainingbatch2/Demo02.java
@@ -0,0 +1,14 @@
+public class Demo02 {
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        if(a>b){
+            System.out.println("The greatest number is a");
+        }
+        else{
+            System.out.println("The greatest number is b");
+        }
+        }
+    }
+    
+
