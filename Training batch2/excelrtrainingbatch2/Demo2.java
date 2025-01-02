0b7b0dc365bdd4fe7013dec0591032771c76a257
meghanareddytenali <tenalimeghana000@gmail.com>
2025-01-02 12:01:55 +0530

    Update Demo2.java

--- Training batch2/excelrtrainingbatch2/Demo2.java
+++ Training batch2/excelrtrainingbatch2/Demo2.java
@@ -1,1 +1,14 @@
+public class Demo2 {
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        if(a>b){
+            System.out.println("The greatest number is a");
+        }
+        else{
+            System.out.println("The greatest number is b");
+        }
+        }
+    }
+    
 
