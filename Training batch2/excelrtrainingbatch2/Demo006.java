39448db1c9f2a00140b279f0186ca94c02f49ef4
meghanareddytenali <tenalimeghana000@gmail.com>
2025-01-03 10:12:04 +0530

    Add files via upload

---
+++ Training batch2/excelrtrainingbatch2/Demo006.java
@@ -0,0 +1,10 @@
+public class Demo006 {
+    public static void main(String[] args){
+        String username = "meghana";
+        String pwd = "meghana@123";
+        while((username == "meghana") && (pwd == "meghana@123")) {
+            System.out.println("hello user Happy new year");
+            break;
+        }
+        }
+}
