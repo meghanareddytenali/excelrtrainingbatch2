9922a5dbddec98ea101e944be9c7a278945dbf9c
meghanareddytenali <tenalimeghana000@gmail.com>
2025-02-05 10:18:27 +0530

    Add files via upload

---
+++ Training batch2/excelrtrainingbatch2/Demo066.java
@@ -0,0 +1,12 @@
+public class Demo066 {
+    public static void main(String[] args) {
+       
+        int[] array = {10, 20, 30, 40, 50};
+
+        
+        System.out.println("Array Index Values:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Index " + i + ": " + array[i]);
+        }
+    }
+}
