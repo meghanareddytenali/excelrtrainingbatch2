5823c133c18208c55d01502cc83f9a0ea6791d32
meghanareddytenali <tenalimeghana000@gmail.com>
2025-02-05 09:55:52 +0530

    Add files via upload

---
+++ Training batch2/excelrtrainingbatch2/Demo061.java
@@ -0,0 +1,20 @@
+public class Demo061 {
+    public static void main(String[] args) {
+        System.out.println("Prime numbers from 1 to 10:");
+        
+        for (int num = 2; num <= 10; num++) {
+            boolean isPrime = true;
+            
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            
+            if (isPrime) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+}
